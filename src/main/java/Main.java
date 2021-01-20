72061863591aeb580f1d23694eb733d8380393bd
evsyukoov <89853197824@mail.ru>
2021-01-20 12:19:43 +0300

    find cartographic library

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import org.osgeo.proj4j.ProjCoordinate;
+import org.osgeo.proj4j.datum.Ellipsoid;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        ProjCoordinate projCoordinate = new ProjCoordinate();
+    }
+}
