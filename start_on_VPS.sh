180e0b8074b686cbf0df0912f0834fddb110cad4
evsyukoov <89853197824@mail.ru>
2021-08-08 17:53:56 +0300

    start scrypt

--- start_on_VPS.sh
+++ start_on_VPS.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+#для старта на сервере
 mkdir logs
 mkdir src/main/resources/uploaded
 mkdir src/main/resources/send
 
 sed  -i 's_Users/denis/Denis/DxfParser_home/denis/DxfConverter/converter_g' src/main/java/convert/DXFConverter.java
+sh ../start_scrypt.sh
 mvn package
 nohup java -jar target/TransformBot-1.0-SNAPSHOT.jar &
