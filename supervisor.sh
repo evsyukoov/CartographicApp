343acca59aca451aad3fef9642d7c10803d51c91
evsyukoov <89853197824@mail.ru>
2021-08-16 22:35:51 +0300

    bash script to fix unexpected fails

--- supervisor.sh
+++ supervisor.sh
@@ -1,23 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
+touch supervisor_log
+echo "Monitoring start" >> supervisor_log
 while true
-do
-  process1=$(ps aux | grep -v grep | grep target/IngGeoBot-1.0.jar)
-  process2=$(ps aux | grep -v grep | grep target/GeodeticBot-1.0.jar)
+   do
+   process1=$(ps aux | grep -v grep | grep IngGeoBot-1.0.jar)
+   process2=$(ps aux | grep -v grep | grep GeodeticBot-1.0.jar)
 
-  if [ -z "$process1" ];then
-    NOW=$(date +"%Y-%m-%d;%H-%M-%S")
-    echo "$NOW"
-    echo "IngGeoBot was failed..restarting..."
-    nohup java -jar target/IngGeoBot-1.0.jar &
-  fi
+    if [ -z "$process1" ];then
+      NOW=$(date +"%Y-%m-%d;%H-%M-%S")
+      echo "$NOW" >> supervisor_log
+      echo "IngGeoBot was failed..restarting..." >> supervisor_log
+      cd CartographicApp2 && nohup java -jar target/IngGeoBot-1.0.jar &
+      cd ~/
+    fi
 
-  if [ -z "$process2" ];then
-    NOW=$(date +"%Y-%m-%d;%H-%M-%S")
-    echo "$NOW"
-    echo "GeodeticBot was failed..restarting..."
-    nohup java -jar target/GeodeticBot-1.0.jar &
-  fi
+    if [ -z "$process2" ];then
+      NOW=$(date +"%Y-%m-%d;%H-%M-%S")
+      echo "$NOW" >> supervisor_log
+      echo "GeodeticBot was failed..restarting..." >> supervisor_log
+      cd CartographicApp && nohup java -jar target/GeodeticBot-1.0.jar &
+      cd ~/
+    fi
 
-  sleep 60
-done
+    sleep 60
+ done
